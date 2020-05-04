ca9da30e1335870f125a95da62872c65c2820051
savicci <koziol.grzesiek@interia.pl>
2020-05-04 12:13:20 +0200

    initial commit

---
+++ src/main/java/com/koziol/passmanager/PassmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.koziol.passmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassmanagerApplication.class, args);
+    }
+
+}
